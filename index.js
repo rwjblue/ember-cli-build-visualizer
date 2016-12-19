309ed77ba78c249085643e26a7b3cf44f6568399
Robert Jackson <rjackson@linkedin.com>
2016-12-18 23:56:28 -0500

    Add basic structure to catch hook and warn when not invoked.
    This uses a feature detect instead of ember-cli-version-checker,
    since it would be hard to detect canary only builds (which is the
    only place the experiment is present).

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-/* jshint node: true */
 'use strict';
 
+const BUILD_INSTRUMENTATION = (function() {
+  try {
+    let experiments = require('ember-cli/lib/experiments');
+
+    return experiments.BUILD_INSTRUMENTATION || 'buildInstrumentation';
+  } catch(e) {
+    // just return `buildInstrumentation` regardless
+    // we make sure to warn if it isn't used below
+    return 'buildInstrumentation';
+  }
+})();
+
 module.exports = {
-  name: 'ember-cli-build-visualizer'
+  name: 'ember-cli-build-visualizer',
+
+  init() {
+    this._super && this._super.init.apply(this, arguments);
+
+    this._hasWarned = false;
+    this._hasBuildInstrumentationFired = false;
+
+    // enable build instrumentation
+    process.env.EMBER_CLI_INSTRUMENTATION = '1';
+  },
+
+  _warnExperimentNotFound() {
+    if (!this._hasWarned) {
+      this._hasWarned = true;
+
+      this.ui.writeLine('Nice message goes here!');
+    }
+  },
+
+  [BUILD_INSTRUMENTATION](buildInstrumentation) {
+    this._hasBuildInstrumentationFired = true;
+    this.ui.writeLine('buildSteps: ' + buildInstrumentation.summary.buildSteps);
+  },
+
+  outputReady() {
+    // confirm that build instrumentation hook was fired
+    // this lets us detect and warn when used before
+    // the hook was added
+    if (!this._hasBuildInstrumentationFired) {
+      this._warnExperimentNotFound();
+    }
+  }
 };
